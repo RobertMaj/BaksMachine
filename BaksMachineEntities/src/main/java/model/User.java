921d3dc5a7b1fd7916d11451ebe164cb7e1626b2
RobertMaj <majrobert90@gmail.com>
2015-10-19 23:50:06 +0200

    user

---
+++ BaksMachineEntities/src/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.io.Serializable;
+
+/**
+ * Created by RobertM on 2015-10-19.
+ */
+public class User implements Serializable {
+
+    private Integer id;
+    private String name;
+    private String surname;
+
+}
