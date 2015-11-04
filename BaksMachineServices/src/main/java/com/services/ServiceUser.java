b958c168cf2246f2ada34c4a8ee2603bd0d15513
RobertMaj <majrobert90@gmail.com>
2015-11-04 14:35:47 +0100

    pierwsze klasy

---
+++ BaksMachineServices/src/main/java/com/services/ServiceUser.java
@@ -0,0 +1,7 @@
+package com.services;
+
+/**
+ * Created by Robert on 2015-11-04.
+ */
+public class ServiceUser {
+}
---
+++ BaksMachineWebApp/src/main/java/com/app/mainConfig/AppConfig.java
@@ -0,0 +1,8 @@
+package com.app.mainConfig;
+
+/**
+ * Created by Robert on 2015-11-04.
+ */
+
+public class AppConfig {
+}
