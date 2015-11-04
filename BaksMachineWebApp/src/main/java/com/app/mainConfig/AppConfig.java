28e2e1a4ec4a47fc6c00f08d063222768eb8bb69
RobertMaj <majrobert90@gmail.com>
2015-11-04 15:44:16 +0100

    koniec dnia 04.11

--- BaksMachineWebApp/src/main/java/com/app/mainConfig/AppConfig.java
+++ BaksMachineWebApp/src/main/java/com/app/mainConfig/AppConfig.java
@@ -5,13 +5,21 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by Robert on 2015-11-04.
  */
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
 
     @Bean
@@ -22,6 +30,29 @@ public class AppConfig {
         dataSourceConfig.setUsername(env.getRequiredProperty("baks.db.user"));
         dataSourceConfig.setPassword(env.getRequiredProperty("baks.db.pass"));
         return new HikariDataSource(dataSourceConfig);
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource,
+                                                                    Environment env) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("src.main.java");
+
+        Properties jpaProperties = new Properties();
+
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
     }
 
 }
